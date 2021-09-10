64d84a87718e640bc95ab1389cb623730e3397ee
hunsovanneth123 <hunsovanneth123@gmail.com>
2021-09-10 11:16:21 +0700

    Add Profile

--- resources/views/layouts/navbar.blade.php
+++ resources/views/layouts/navbar.blade.php
@@ -1,7 +1,12 @@
 <nav class="main-header navbar navbar-expand navbar-light">
   <ul class="navbar-nav">
     <li class="nav-item">
-      <a class="nav-link" data-widget="pushmenu" href="#"><i class="fa fa-bars"></i></a>
+      {{-- <a class="nav-link" data-widget="pushmenu" href="#"><i class="fa fa-bars"></i></a> --}}
+      <a class="nav-link" data-widget="pushmenu" href="#" role="button" data-enable-remember="true"><i class="fas fa-bars"></i></a>
+    </li>
+    <li class="nav-item" style="position: absolute; right: 20px">
+      <img src="{{ asset('adminlte/dist/img/avatar/avatar-2.jpg') }}" alt="logo" width="40px" style="border-radius: 50%;border: 2px solid black">
+      <span style="text-transform: uppercase; font-weight: bold"> Hun Sovanneth</span>
     </li>
   </ul>
 </nav>

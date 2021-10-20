91e2ad3a7f4aea854cba462d2d481f07f4ba546c
KienPro <sruo.kien@gmail.com>
2021-10-20 18:47:09 +0700

    Fixed download qr code

--- resources/views/layouts/restaurant_sidebar.blade.php
+++ resources/views/layouts/restaurant_sidebar.blade.php
@@ -195,14 +195,14 @@
     </div>
   </div>  
   <div class="scan-for-menu">
-    <span>Scan For Menu</span><br>
+    {{-- <span>Scan For Menu</span><br> --}}
      <a href="data:image/png;base64, 
           {!! base64_encode(QrCode::format('png')
           ->merge('adminlte/dist/img/logo/emenu-square-black-bg-with-stroke.png', .3, true)
           ->size(300)
           ->errorCorrection('H')
-          ->generate($restaurant_info->website_url ?? '' )) !!} " download="{{ $restaurant_info->name }} .'-qrcode'" style="width: 100%" 
-      class="btn btn-default rounded-pill btn-xs px-2" type="button" title="Download" id="qrdownload">
+          ->generate($restaurant_info->website_url ?? '' )) !!}" download
+      class="btn btn-default rounded-pill btn-xs" type="button" title="Download" id="qrdownload">
       Download <i class="far fa-download text-warning" style="opacity:1;"></i>
     </a>
     <button class="btn btn-default rounded-pill btn-xs px-2" type="button" title="share link" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">

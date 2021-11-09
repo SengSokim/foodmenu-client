9afb1e5e08fe26697ab61d6390b50e65de6e17d2
Chorn Penghong <86304457+ChornPenghong@users.noreply.github.com>
2021-11-09 17:21:47 +0700

    generate vue code from resource to public

--- webpack.mix.js
+++ webpack.mix.js
@@ -36,6 +36,10 @@ mix.js('resources/assets/js/app.js', 'public/dist/js')
     //telegram
     .js('resources/assets/js/setting/telegram/telegram.js', 'public/dist/js/setting')
 
+    //users
+    .js('resources/assets/js/users/user.js', 'public/dist/js/users')
+
+
 
 
 

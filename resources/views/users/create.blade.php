01c2f208ae2cf59af7831c35cd42ddb9d3a2ea58
Chorn Penghong <86304457+ChornPenghong@users.noreply.github.com>
2021-11-09 17:17:41 +0700

    Add form.blade to user

--- resources/views/users/create.blade.php
+++ resources/views/users/create.blade.php
@@ -1,51 +1,21 @@
-<form method="GET">
-    <div class="modal fade" id="createUser" tabindex="-1" role="dialog" aria-hidden="true">
-        <div class="modal-dialog" role="document">
-            <div class="modal-content">
-                <div class="modal-header">
+<div class="modal fade" id="createUser" tabindex="-1" role="dialog" aria-hidden="true">
+    <div class="modal-dialog" role="document">
+        <div class="modal-content">
+            <div class="modal-header">
                 <h5 class="modal-title"><i class="fas fa-user"></i> Create User</h5>
                 <button type="button" class="close" data-dismiss="modal" aria-label="Close">
                     <span aria-hidden="true">&times;</span>
                 </button>
-                </div>
-                <div class="modal-body">
-                    <div class="form-group">
-                        <label>Name</label>
-                        <input type="text" class="form-control" placeholder="User Name..." required>
-                    </div>
-                    <div class="form-group">
-                        <label>Gender</label>
-                        <select class="form-control">
-                            <option value="">Select Gender</option>
-                            <option value="male">Male</option>
-                            <option value="female">Female</option>
-                        </select>
-                    </div>
-                    <div class="form-group">
-                        <label>Phone Number</label>
-                        <input type="tel" class="form-control" placeholder="Phone Number..." required>
-                    </div>
-                    <div class="form-group">
-                        <label>Role</label>
-                        <select class="form-control">
-                            <option value="">Select Role</option>
-                            <option value="admin">Admin</option>
-                        </select>
-                    </div>
-                    <div class="form-group">
-                        <label>Address</label>
-                        <textarea  rows="3" class="form-control" placeholder="Address..."></textarea>
-                    </div>
-                    <div class="form-group m-0">
-                        <input type="checkbox">
-                        <label for="status">Active</label>
-                    </div>
-                </div>
-                <div class="modal-footer">
-                    <button type="button" class="btn btn-danger btn-sm" data-dismiss="modal"><i class="fas fa-ban"></i> Close</button>
-                    <button type="sumbit" class="btn btn-primary btn-sm"><i class="fas fa-save"></i> Save</button>
-                </div>
+            </div>
+            <div class="modal-body">
+                <form id="createUser">
+                    @include('users.form')
+                </form>
+            </div>
+            <div class="modal-footer">
+                <button type="button" class="btn btn-default rounded-pill" data-dismiss="modal" >Cancel</button>
+                <button type="sumbit" class="btn btn-warning rounded-pill"> Save</button>
             </div>
         </div>
     </div>
-</form>
+</div>
---
+++ resources/views/users/form.blade.php
@@ -0,0 +1,31 @@
+<div class="form-group">
+    <label>Username</label>
+    <input type="text" class="form-control" placeholder="User Name..." required>
+</div>
+<div class="form-group">
+    <label>Gender</label>
+    <select class="form-control">
+        <option value="">Select Gender</option>
+        <option value="male">Male</option>
+        <option value="female">Female</option>
+    </select>
+</div>
+<div class="form-group">
+    <label>Phone Number</label>
+    <input type="tel" class="form-control" placeholder="Phone Number..." required>
+</div>
+<div class="form-group">
+    <label>Role</label>
+    <select class="form-control">
+        <option value="">Select Role</option>
+        <option value="admin">Admin</option>
+    </select>
+</div>
+<div class="form-group">
+    <label>Address</label>
+    <textarea  rows="3" class="form-control" placeholder="Address..."></textarea>
+</div>
+<div class="form-group m-0">
+    <input type="checkbox">
+    <label for="status">Active</label>
+</div>

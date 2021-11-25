4f74f5feead9c161072aba81ba2a71da91a33fdd
KienPro <sruo.kien@gmail.com>
2021-11-25 16:20:04 +0700

    IMP telegram setting

--- resources/assets/js/setting/telegram/telegram.js
+++ resources/assets/js/setting/telegram/telegram.js
@@ -29,6 +29,30 @@ new Vue({
         })
     },
     methods: {
+
+        preview(param){
+            switch (param) {
+                case 'user':
+                    var URL=this.data.telegram_user;
+                    if (URL.indexOf("https://t.me/") > -1) {
+                        var urlSplit=URL.split('/');
+                        window.open('https://t.me/' + urlSplit[3], '_blank');
+                    }else
+                    window.open('https://t.me/' + this.data.telegram_user, '_blank');
+                    break;
+                case 'group':
+                    var URL=this.data.telegram_group;
+                    if (URL.indexOf("https://t.me/") > -1) {
+                        var urlSplit=URL.split('/');
+                        window.open('https://t.me/' + urlSplit[3], '_blank');
+                    }else
+                    window.open('https://t.me/' + this.data.telegram_group, '_blank');
+                    break;
+                default:
+                    window.open('https://web.telegram.org/', '_blank');
+                    break;
+            }
+        },
         save(){
             showLoading()
             axios.post(`/admin/restaurants`,
@@ -60,5 +84,9 @@ new Vue({
                 }
             })
         },
+
+        created(){
+            return  this.data.telegram_user ==null;
+        }
     }
 });
--- resources/views/setting/telegram/index.blade.php
+++ resources/views/setting/telegram/index.blade.php
@@ -10,49 +10,44 @@
         Setting up Telegram
       </div>
       <div class="card-body" id="telegram"  v-cloak>
-          <div class="row">
-              <div class="col-md-6">
-                <div class="form-group">
-                  <h5>Telegram</h5>
-                  <p>{{ __('app.setting.objective-setup-telegram') }}</p>
-                </div>
-              </div>
-              <div class="col-md-6 pr-5">
-                <h5>{{ __('app.setting.create-telegram-group') }}</h5>
-                <p>{{ __('app.setting.you-can-create-telegram-group-by-click-this-link') }}:<a href="https://web.telegram.org/" class="btn btn-link" target="_blank">Telegram <i class="fab fa-telegram"></i></a></p>
-                <form @submit.prevent="submit" v-cloak>
-                  <div class="row">
-                    <div class="col-md-12">
-                      <div class="form-group">
-                        <label for="telgram_user">{{ __('app.setting.telegram-user') }}</label>
-                        <input type="text" class="form-control" name="telegram_user" v-model="data.telegram_user" placeholder="e.g. telegram_user">
+        <div class="row">
+          <div class="col-md-6">
+            <div class="form-group">
+              <h5>Telegram</h5>
+              <p>{{ __('app.setting.objective-setup-telegram') }}</p>
+            </div>
+          </div>
+          <div class="col-md-6 pr-5">
+            <h5>{{ __('app.setting.create-telegram-group') }}</h5>
+            <p>{{ __('app.setting.you-can-create-telegram-group-by-click-this-link') }}:<a href="https://web.telegram.org/" class="btn btn-link" target="_blank">Telegram <i class="fab fa-telegram"></i></a></p>
+            <form @submit.prevent="submit" v-cloak>
+              <div class="row">
+                <div class="col-md-12">
+                  <div class="form-group">
+                    <label for="telgram_user">{{ __('app.setting.telegram-user') }}</label>
+                    <div class="input-group">
+                      <input type="text" class="form-control" name="telegram_user" v-model="data.telegram_user" placeholder="e.g. telegram_user">
+                      <div class="input-group-append" title="Preview">
+                        <span class="input-group-text bg-transparent" @click="preview('user')" style="cursor: pointer"><i class="fas fa-eye"></i></span>
                       </div>
-                      <div class="form-group">
-                        <label for="telgram_user">{{ __('app.setting.telegram-group') }}</label>
-                        <input type="text" class="form-control" name="telegram_group" v-model="data.telegram_group"  placeholder="e.g. telegram_group">
-                      </div>
-                      <button type="submit" class="btn btn-warning btn-sm rounded-pill" v-if="telegramData.telegram_user === null && telegramData.telegram_group === null">{{ __('app.global.save') }}</button>
-                      <button type="submit" class="btn btn-warning btn-sm rounded-pill" v-else>{{ __('app.global.save-changes') }}</button>
                     </div>
                   </div>
-                </form>
-                <hr>
-                <h5>{{ __('app.setting.your-telegram') }}</h5>
-                <div>
-                  <b>{{ __('app.setting.telegram-user') }} &nbsp;&nbsp;&nbsp;:</b>
-                  <a :href="'https://t.me/' + telegramData.telegram_user" style="color: #FFB300" target="_blank" v-if="telegramData.telegram_user">
-                    <span>@{{ telegramData.telegram_user}}</span> 
-                  </a> 
-                  <span v-else class="text-muted">{{ __('app.setting.not-set') }}</span>
-                  <br>
-                  <b>{{ __('app.setting.telegram-group') }} :</b> 
-                  <a :href="'https://t.me/' + telegramData.telegram_group" style="color: #FFB300" target="_blank" v-if="telegramData.telegram_group">
-                    <span>@{{ telegramData.telegram_group  ?? 'Not Set' }}</span> 
-                  </a>
-                   <span v-else class="text-muted">{{ __('app.setting.not-set') }}</span>
+                  <div class="form-group">
+                    <label for="telgram_user">{{ __('app.setting.telegram-group') }}</label>
+                    <div class="input-group">
+                      <input type="text" class="form-control" name="telegram_group" v-model="data.telegram_group"  placeholder="e.g. telegram_group">
+                      <div class="input-group-append" title="Preview">
+                        <span class="input-group-text bg-transparent" @click="preview('group')" style="cursor: pointer"><i class="fas fa-eye"></i></span>
+                      </div>
+                    </div>
+                  </div>
+                  <button type="submit" class="btn btn-warning btn-sm rounded-pill" v-if="telegramData.telegram_user === null && telegramData.telegram_group === null">{{ __('app.global.save') }}</button>
+                  <button type="submit" class="btn btn-warning btn-sm rounded-pill" v-else>{{ __('app.global.save-changes') }}</button>
                 </div>
               </div>
+            </form>
           </div>
+        </div>
       </div>
     </div>
   </div>

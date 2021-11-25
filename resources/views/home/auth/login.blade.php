355b1868984874d2c1e21e1b135393ddb021b1bc
Chorn Penghong <86304457+ChornPenghong@users.noreply.github.com>
2021-11-25 13:44:15 +0700

    Imp Favicon on login

--- resources/views/home/auth/login.blade.php
+++ resources/views/home/auth/login.blade.php
@@ -5,9 +5,8 @@
   <meta name="viewport" content="width=device-width, initial-scale=1.0,maximum-scale=1.0, user-scalable=0">
   <meta http-equiv="X-UA-Compatible" content="ie=edge">
   <title>eMenu</title>
-
   <!-- Favicon Icon -->
-  <link rel="shortcut icon" type="image/x-icon" href="{{ asset('adminlte/dist/img/logo/emenu-square-black-bg.png') }}">
+  <link rel="shortcut icon" type="image/x-icon" href="{{ asset('adminlte/dist/img/logo/emenu-square-black-bg-with-no-stroke.png') }}">
   <link href="https://fonts.googleapis.com/css?family=Karla:400,700&display=swap" rel="stylesheet">
   <link rel="stylesheet" href="https://cdn.materialdesignicons.com/4.8.95/css/materialdesignicons.min.css">
   <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/css/bootstrap.min.css">

8aededcc2add67ef8b146fed357f7bee54e40486
Chorn Penghong <86304457+ChornPenghong@users.noreply.github.com>
2021-12-15 16:40:16 +0700

    Add video tutorial url( how to PAPA eMenu)

--- resources/views/layouts/master.blade.php
+++ resources/views/layouts/master.blade.php
@@ -27,7 +27,7 @@
 
       <footer class="main-footer text-sm" >
         <div class="clearfix">
-          <a href="https://www.youtube.com/watch?v=KYHOm7V2H5Q&t=5s" target="_blank" class="btn btn-link btn-sm rounded-pill" style="color: #1aa7ec"><i class="fas fa-video"></i> {{ __('app.footer.video-tutorials') }}</a> | 
+          <a href="https://www.youtube.com/watch?v=Q742xYQAKnM" target="_blank" class="btn btn-link btn-sm rounded-pill" style="color: #1aa7ec"><i class="fas fa-video"></i> {{ __('app.footer.video-tutorials') }}</a> | 
           <a href="#" class="btn btn-link btn-sm rounded-pill" style="color: #1aa7ec"><i class="fas fa-book"></i> {{ __('app.footer.help-docs') }}</a>
         </div>
       </footer>

481ec60cd2ddf0269405db5534c2174ecac0eb43
Chorn Penghong <86304457+ChornPenghong@users.noreply.github.com>
2021-12-17 16:51:32 +0700

    IMP localization and Table UI

--- resources/views/users/table.blade.php
+++ resources/views/users/table.blade.php
@@ -16,8 +16,8 @@
       <tr class="text-center" v-for="(item, index) in restaurant_users.data">
         <td>@{{ index + 1 }}</td>
         <td><img :src="item.media.url" class="user_image"></td>
-        <td>@{{ item.name}}</td>
-        <td >@{{ item.gender }}</td>
+        <td class="text-left">@{{ item.name}}</td>
+        <td style="text-transform: capitalize">@{{ item.gender }}</td>
         <td>@{{ item.phone_number }}</td>
         <td style="text-transform: capitalize">@{{ item.role }}</td>
         <td>@{{ item.enable_status ? 'Active' : 'Deactive' }}</td>

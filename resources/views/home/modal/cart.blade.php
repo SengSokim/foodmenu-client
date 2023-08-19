0ca6b8edf89e72b46420d16da16722021b2c5d22
PUTSOKLEAP <sokleaptorng716@gmail.com>
2023-08-19 21:20:27 +0700

    IMP UI And Responsive

--- resources/views/home/modal/cart.blade.php
+++ resources/views/home/modal/cart.blade.php
@@ -1,4 +1,4 @@
-<div class="modal fade" id="cart-modal" tabindex="-1" aria-hidden="true">
+<!-- <div class="modal fade" id="cart-modal" tabindex="-1" aria-hidden="true">
     <div class="modal-dialog modal-lg">
         <div class="modal-content">
             <div class="modal-header">
@@ -38,10 +38,10 @@
                             <img :src="item.img" alt="" width="50px" height="50px">
                             <div class="ml-2">
                                 @{{ item.name }} <br>
-                                <span class="text-secondary" >@{{ item.code }}</span>
-                                
+                                <span class="text-secondary">@{{ item.code }}</span>
+
                             </div>
-                            
+
                         </div>
                         <div class="col-md-3 text-center">
                             @{{ formatCurrency(item.price) }}
@@ -77,13 +77,73 @@
             </form>
         </div>
     </div>
+</div> -->
+<div class="modal fade" id="cart-modal" tabindex="-1" aria-hidden="true">
+    <div class="modal-dialog" role="document">
+        <div class="modal-content">
+            <div class="modal-header">
+                <h4 style="color: #154d97;" class="modal-title text-bold" id="orderModalLabel">
+                    <i class="fas fa-shopping-cart mr-2"></i>Cart Items
+                </h4>
+                <button type="button" class="close" data-dismiss="modal" aria-label="Close" @click="clear()">
+                    <span aria-hidden="true">
+                        <i class="far fa-times fa-fw text-secondary"></i>
+                    </span>
+                </button>
+            </div>
+            <div class="modal-body">
+                <table class="table table-striped table-borderless">
+                    <thead>
+                        <tr>
+                            <th style="color: #154d97;"><i class="fas fa-tag mr-1"></i> Item</th>
+                            <th style="color: #154d97;"><i class="fas fa-dollar-sign mr-1"></i> Price</th>
+                            <th style="color: #154d97;"><i class="fas fa-sort-numeric-up mr-1"></i> Quantity</th>
+                            <th style="color: #154d97;"><i class="fas fa-money-bill mr-1"></i> Total</th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                        <tr>
+                            <td>Item 1</td>
+                            <td>$10</td>
+                            <td>2</td>
+                            <td>$20</td>
+                        </tr>
+                        <tr>
+                            <td>Item 2</td>
+                            <td>$15</td>
+                            <td>3</td>
+                            <td>$45</td>
+                        </tr>
+                        <tr>
+                            <td>Item 3</td>
+                            <td>$5</td>
+                            <td>1</td>
+                            <td>$5</td>
+                        </tr>
+                    </tbody>
+                </table>
+            </div>
+            <div class="modal-footer">
+                <h5 class="mr-auto">
+                    <strong>
+                        <i class="fas fa-coins mr-1"></i>Grand Total: $70
+                    </strong>
+                </h5>
+                <button type="button" class="btn btn-danger" data-dismiss="modal"><i
+                        class="fas fa-times mr-1"></i>Cancel</button>
+                <button type="button" class="btn btn-primary" data-toggle="modal" data-target="#myModal"
+                    style="background-color: #154d97;"><i class="fas fa-check mr-1"></i>Checkout</button>
+            </div>
+        </div>
+    </div>
 </div>
+
 <div class="modal fade" id="myModal" tabindex="-1" aria-hidden="true">
     <div class="modal-dialog">
         <div class="modal-content">
             <div class="modal-header">
-                <h4 class="modal-title">
-                    <i class="fas fa-info-circle"></i> Enter Details
+                <h4 style="color: #154d97;" class="modal-title">
+                    <i class="fas fa-info-circle mr-2"></i>Enter Details
                 </h4>
                 <button type="button" class="close" data-dismiss="modal" aria-label="Close" @click="clear()">
                     <span aria-hidden="true">
@@ -95,19 +155,19 @@
                 <form>
                     <div class="form-group">
                         <label for="name">
-                            <i class="fas fa-user"></i> Name <span class="text-danger">*</span>
+                            <i class="fas fa-user mr-1"></i>Name <span class="text-danger">*</span>
                         </label>
                         <input type="text" class="form-control" id="customer-name" v-model="orderDetails.customer_name">
                     </div>
                     <div class="form-group">
                         <label for="phone">
-                            <i class="fas fa-phone"></i> Phone Number <span class="text-danger">*</span>
+                            <i class="fas fa-phone mr-1"></i>Phone Number <span class="text-danger">*</span>
                         </label>
                         <input type="text" class="form-control" id="customer-phone" v-model="orderDetails.phone_number">
                     </div>
                     <div class="form-group">
                         <label for="address">
-                            <i class="fas fa-map-marker-alt"></i> Address
+                            <i class="fas fa-map-marker-alt mr-1"></i>Address
                         </label>
                         <input type="text" class="form-control" id="customer-address" v-model="orderDetails.address">
                     </div>
@@ -115,17 +175,82 @@
             </div>
             <div class="modal-footer">
                 <button type="button" class="btn btn-danger" data-dismiss="modal">
-                    <i class="far fa-times-circle"></i> Cancel
+                    <i class="far fa-times-circle mr-1"></i>Cancel
                 </button>
                 <button type="button" class="btn btn-warning" id="backButton">
-                    <i class="fas fa-arrow-left"></i> Back
+                    <i class="fas fa-arrow-left mr-1"></i>Back
                 </button>
-                <button type="button" class="btn btn-primary" style="background-color: #154d97;" @click="createOrder">
-                    <i class="far fa-check-circle"></i> Submit
+                <button data-toggle="modal" data-target="#invoiceModal" type="button" class="btn btn-primary"
+                    style="background-color: #154d97;" @click="createOrder" data-dismiss="modal" id="submitBtn">
+                    <i class="far fa-check-circle mr-1"></i>Submit
                 </button>
             </div>
         </div>
     </div>
 </div>
 
-
+<div class="modal fade" id="invoiceModal" tabindex="-1" role="dialog" aria-labelledby="invoiceModalLabel"
+    aria-hidden="true" style="background-color: rgba(0, 0, 0, 0.5);">
+    <div class="modal-dialog" role="document">
+        <div class="modal-content">
+            <div class="modal-header">
+                <h4 style="color: #154d97;" class="modal-title text-bold" id="invoiceModalLabel">
+                    <i class="fas fa-file-invoice-dollar mr-2"></i>Invoice
+                </h4>
+                <button type="button" class="close" data-dismiss="modal" aria-label="Close" @click="clear()">
+                    <span aria-hidden="true">
+                        <i class="far fa-times fa-fw text-secondary"></i>
+                    </span>
+                </button>
+            </div>
+            <div class="modal-body">
+                <table class="table table-striped table-borderless">
+                    <thead>
+                        <tr>
+                            <th style="color: #154d97;"><i class="fas fa-utensils mr-1"></i>Name</th>
+                            <th style="color: #154d97;"><i class="fas fa-dollar-sign mr-1"></i>Price</th>
+                            <th style="color: #154d97;"><i class="fas fa-sort-numeric-up mr-1"></i>Quantity</th>
+                            <th style="color: #154d97;"><i class="fas fa-dollar-sign mr-1"></i>Sub Total</th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                        <tr>
+                            <td>Burger</td>
+                            <td>$5.99</td>
+                            <td>2</td>
+                            <td>$11.98</td>
+                        </tr>
+                        <tr>
+                            <td>Pizza</td>
+                            <td>$8.99</td>
+                            <td>1</td>
+                            <td>$8.99</td>
+                        </tr>
+                    </tbody>
+                </table>
+                <div class="text-left" style="display: flex; justify-content: space-between;">
+                    <div>
+                        <h5>
+                            <strong><i class="fas fa-phone mr-1"></i>Phone:</strong>
+                            <span>010223344</span>
+                        </h5>
+                    </div>
+                    <div>
+                        <h5 class="mr-auto">
+                            <strong>
+                                <i class="fas fa-coins mr-1"></i>Grand Total: $7023
+                            </strong>
+                        </h5>
+                    </div>
+                </div>
+            </div>
+            <div class="modal-footer">
+                <button type="button" class="btn btn-danger" data-dismiss="modal"><i
+                        class="far fa-times-circle"></i>Close</button>
+                <button type="button" style="background-color: #154d97;" class="btn btn-primary">
+                    <i class="fas fa-download mr-1"></i>Download
+                </button>
+            </div>
+        </div>
+    </div>
+</div>
--- resources/views/home/modal/productdetails.blade.php
+++ resources/views/home/modal/productdetails.blade.php
@@ -2,7 +2,7 @@
     <div class="modal-dialog">
         <div class="modal-content">
             <div class="modal-header">
-                <h5 class="modal-title text-bold">
+                <h5 style="color: #154d97;" class="modal-title text-bold">
                     <i class="fas fa-info-circle fa-fw"></i> Product Detail
                 </h5>
                 <button type="button" class="close" data-dismiss="modal" aria-label="Close" @click="clear()">
@@ -11,57 +11,47 @@
                     </span>
                 </button>
             </div>
-           
-                @csrf
-                <div class="modal-body">
-                    <div class="card-image-container">
-                        <img class="card-image" :src="productDetail.image?.url" alt="img" width="200px" height="300px">
-                    </div>
-                    <div class="card-detail">
-                        <span class="menu-name">
-                            <i class="fas fa-tags fa-fw"></i> @{{ productDetail.name }}
-                        </span>
-                        <span class="main-price">
-                            <i class="fas fa-dollar-sign fa-fw"></i> @{{ formatCurrency(productDetail.price) }}
-                        </span>
-                        <hr class="hr-style">
-                        <p>
-                            @{{ productDetail.description }}
-                        </p>
-                        <hr class="hr-style">
-                        <h3>
-                            <i class="fas fa-dollar-sign fa-fw"></i> Total:
-                            <span>@{{ formatCurrency(productDetail.price * toAdd.qty) }}</span>
-                        </h3>
-                        <hr class="hr-style">
-                        <div class="d-flex justify-content-between">
-                            <div class="quantity">
-                                <button type="button" @click="decrement()">
-                                    <i class="fas fa-minus"></i>
-                                </button>
-                                <span id="quantityDisplay">@{{ toAdd.qty }}</span>
-                                <button type="button" class="increase-btn" @click="increment()">
-                                    <i class="fas fa-plus"></i>
-                                </button>
-                            </div>
-                            <button class="card-button" type="button" @click="addToCart(productDetail.id)">
-                                <span class="icon">
-                                    <i class="fas fa-cart-arrow-down"></i>
-                                </span>
-                                <span class="text">Add to cart</span>
+
+            @csrf
+            <div class="modal-body">
+                <div class="card-image-container">
+                    <img class="card-image" :src="productDetail.image?.url" alt="img" width="200px" height="300px">
+                </div>
+                <div class="card-detail">
+                    <span class="menu-name">
+                        <i class="fas fa-tags fa-fw"></i> @{{ productDetail.name }}
+                    </span>
+                    <span class="main-price">@{{ formatCurrency(productDetail.price) }}
+                    </span>
+                    <hr class="hr-style">
+                    <p class="popup-descrip-product">
+                        @{{ productDetail.description }}
+                    </p>
+                    <hr class="hr-style">
+                    <h4 class="text-center">
+                        <i class="fas fa-dollar-sign fa-fw popup-dolar"></i>Total:
+                        <span>@{{ formatCurrency(productDetail.price * toAdd.qty) }}</span>
+                    </h4>
+                    <hr class="hr-style">
+                    <div class="d-flex justify-content-between">
+                        <div class="quantity">
+                            <button type="button" @click="decrement()">
+                                <i class="fas fa-minus"></i>
+                            </button>
+                            <span id="quantityDisplay">@{{ toAdd.qty }}</span>
+                            <button type="button" class="increase-btn" @click="increment()">
+                                <i class="fas fa-plus"></i>
                             </button>
                         </div>
+                        <button class="card-button" type="button" @click="addToCart(productDetail.id)">
+                            <span class="icon">
+                                <i class="fas fa-cart-arrow-down"></i>
+                            </span>
+                            <span class="text">Add to cart</span>
+                        </button>
                     </div>
                 </div>
-                <div class="modal-footer modal-footer-b">
-                    <button type="button" class="btn btn-danger" data-dismiss="modal" click="clear()">
-                        <i class="fas fa-times fa-fw"></i> Cancel
-                    </button>
-                    <button type="button" class="btn btn-dark" style="background-color: #5d5d5d;">
-                        <i class="fas fa-plus-circle"></i> View More
-                    </button>
-                </div>
-            
+            </div>
         </div>
     </div>
 </div>

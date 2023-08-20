1e912ef784bd6d5569c0f70503afd5fb69268826
PUTSOKLEAP <sokleaptorng716@gmail.com>
2023-08-20 09:01:49 +0700

    IMP

--- resources/views/home/layouts/master.blade.php
+++ resources/views/home/layouts/master.blade.php
@@ -1,24 +1,26 @@
 @include('home.layouts.master_header')
 
-<body >
+<body>
     @yield('content')
-    <footer>
-        <div class="describe-container">
-            <img class="footer-logo" src="{{ asset('images/amboja_logo.jpg') }}" alt="ambojaLogo">
-            <p class="footer-text-describe">
-                The easiest way to create a digital food menu for your restaurant, bar or cafe.
-            </p>
-        </div>
-        <div class="privacy-container">
-            <div class="support-container">
-                <i class="fa-regular fa-envelope"></i>
-                AMBOJA.com
+    <section class="footer-section">
+        <footer>
+            <div class="describe-container">
+                <img class="footer-logo" src="{{ asset('images/amboja_logo.jpg') }}" alt="ambojaLogo">
+                <p class="footer-text-describe">
+                    The easiest way to create a digital food menu for your restaurant, bar or cafe.
+                </p>
+            </div>
+            <div class="privacy-container">
+                <div class="support-container">
+                    <i class="fa-regular fa-envelope"></i>
+                    AMBOJA.com
+                </div>
+            </div>
+        </footer>
+        <div class="bottom-footer-container">
+            <div class="bottom-footer">
+                @ 2023 - AMBOJA
             </div>
         </div>
-    </footer>
-    <div class="bottom-footer-container">
-        <div class="bottom-footer">
-            @ 2023 - AMBOJA
-        </div>
-    </div>
+    </section>
     @include('home.layouts.master_footer')
--- resources/views/home/modal/cart.blade.php
+++ resources/views/home/modal/cart.blade.php
@@ -1,83 +1,3 @@
-<!-- <div class="modal fade" id="cart-modal" tabindex="-1" aria-hidden="true">
-    <div class="modal-dialog modal-lg">
-        <div class="modal-content">
-            <div class="modal-header">
-                <h5 class="modal-title text-bold" style="color:#154d97">
-                    <i class="fas fa-shopping-cart fa-fw"></i> Order Detail
-                </h5>
-                <button type="button" class="close" data-dismiss="modal" aria-label="Close">
-                    <span aria-hidden="true">
-                        <i class="far fa-times fa-fw text-secondary"></i>
-                    </span>
-                </button>
-            </div>
-            <form action="" method="POST">
-                @csrf
-                <div class="modal-body">
-                    <div class="row align-items-center" style="color:#154d97">
-                        <div class="col-md-3">
-                            <i class="fas fa-tags fa-fw"></i> Item
-                        </div>
-                        <div class="col-md-3 text-center">
-                            <i class="fas fa-dollar-sign fa-fw"></i> Price
-                        </div>
-                        <div class="col-md-3 text-center">
-                            <i class="fas fa-sort-numeric-up fa-fw"></i> Quantity
-                        </div>
-                        <div class="col-md-3">
-                            <span class="float-right">
-                                <i class="fas fa-dollar-sign fa-fw"></i> Total
-                            </span>
-                        </div>
-                    </div>
-                    <hr>
-
-                    <div class="row align-items-center my-1" v-for="item in carts">
-
-                        <div class="col-md-3 d-flex">
-                            <img :src="item.img" alt="" width="50px" height="50px">
-                            <div class="ml-2">
-                                @{{ item.name }} <br>
-                                <span class="text-secondary">@{{ item.code }}</span>
-
-                            </div>
-
-                        </div>
-                        <div class="col-md-3 text-center">
-                            @{{ formatCurrency(item.price) }}
-                        </div>
-                        <div class="col-md-3 text-center">
-                            @{{ item.qty }}
-                        </div>
-                        <div class="col-md-3">
-                            <span class="float-right">
-                                @{{ formatCurrency(item.subtotal) }}
-                            </span>
-                        </div>
-
-                    </div>
-
-                    <div v-if="carts.length == 0" class="text-center text-bold">No Item in cart!</div>
-                    <hr>
-                    <div class="d-flex justify-content-between">
-                        <h4>Grand Total:</h4>
-                        <div>&nbsp;</div>
-                        <span class="text-bold">@{{ formatCurrency(grandtotal) }}</span>
-                    </div>
-                </div>
-                <div class="modal-footer">
-                    <button type="button" class="btn btn-danger" data-dismiss="modal">
-                        <i class="far fa-times fa-fw"></i> Cancel
-                    </button>
-                    <button type="button" class="btn" data-toggle="modal" data-target="#myModal"
-                        style="background-color:#154d97;color:white">
-                        <i class="far fa-receipt fa-fw"></i> Checkout
-                    </button>
-                </div>
-            </form>
-        </div>
-    </div>
-</div> -->
 <div class="modal fade" id="cart-modal" tabindex="-1" aria-hidden="true">
     <div class="modal-dialog" role="document">
         <div class="modal-content">
@@ -108,9 +28,10 @@
                             <td>@{{ item.qty }}</td>
                             <td> @{{ formatCurrency(item.subtotal) }}</td>
                         </tr>
-                    
+
                     </tbody>
                 </table>
+                <p v-if="carts.length == 0" class="text-center text-bold">No Item in cart!</p>
             </div>
             <div class="modal-footer">
                 <h5 class="mr-auto">
@@ -164,14 +85,14 @@
             </div>
             <div class="modal-footer">
                 <button type="button" class="btn btn-danger" data-dismiss="modal">
-                    <i class="far fa-times-circle mr-1"></i>Cancel
+                    <i class="fas fa-times mr-1"></i>Cancel
                 </button>
                 <button type="button" class="btn btn-warning" id="backButton">
                     <i class="fas fa-arrow-left mr-1"></i>Back
                 </button>
                 <button data-toggle="modal" data-target="#invoiceModal" type="button" class="btn btn-primary"
                     style="background-color: #154d97;" @click="createOrder" data-dismiss="modal" id="submitBtn">
-                    <i class="far fa-check-circle mr-1"></i>Submit
+                    <i class="fas fa-check mr-1"></i>Submit
                 </button>
             </div>
         </div>
@@ -229,7 +150,7 @@
             </div>
             <div class="modal-footer">
                 <button type="button" class="btn btn-danger" data-dismiss="modal"><i
-                        class="far fa-times-circle"></i>Close</button>
+                        class="fas fa-times mr-1"></i>Close</button>
                 <button type="button" style="background-color: #154d97;" class="btn btn-primary">
                     <i class="fas fa-download mr-1"></i>Download
                 </button>

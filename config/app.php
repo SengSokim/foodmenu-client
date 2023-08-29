dd15d2d1e0e0211d56ff60aeff58680d6b82509d
Sokim <sengsokim88@gmail.com>
2023-08-29 16:34:35 +0700

    imp https

--- config/app.php
+++ config/app.php
@@ -52,7 +52,7 @@ return [
     |
     */
 
-    'url' => env('APP_URL', 'http://localhost'),
+    'url' => env('APP_URL', 'https://localhost'),
     'api_url' => env('API_URL', 'https://emenu-development-api.rorean.com/api/'),
     'website_url' => env('WEBSITE_URL', 'https://emenu-development-web.rorean.com'),
 

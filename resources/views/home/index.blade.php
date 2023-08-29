d1ce7c17e3ffffced09a3100af97fe08d2577dbd
Chorn Penghong <pengh202@gmail.com>
2023-08-29 16:50:14 +0700

    Fixed

--- resources/views/home/index.blade.php
+++ resources/views/home/index.blade.php
@@ -1,58 +1,10 @@
 @extends('home.layouts.master')
 @section('content')
 
-<body>
-    <div class="main-container" id="homePage" v-cloak>
-        <nav class="navbar">
-            <div class="navbar-brand">
-                <a href="#"><img class="logo" src="{{ asset('images/amboja_logo.jpg') }}" alt="ambojaLogo"></a>
-            </div>
-            <ul class="navbar-menu">
-                <div v-if="carts">
-                    <li>
-                        <button class="add-card-btn" type="button" data-toggle="modal" data-target="#cart-modal">
-                            <i class="fa-solid fa-cart-arrow-down"></i>
-                            <span>Cart</span>
-                            <span class="cart-qty">@{{ carts?.length }}</span>
-                        </button>
-                    </li>
-                </div>
-                <div v-else>
-                    <li><button class="add-card-btn"><i class="fa-solid fa-cart-arrow-down"></i>Add to
-                            cart</button></li>
-                </div>
-            </ul>
-        </nav>
-        <div class="containerr" id="homePage" v-cloak>
-            <div class="food-title-container">
-                <p class="food-title">AMBOJA Food Menu</p>
-            </div>
-
-            <section class="items-section" v-for="list in data">
-                <p class="card-category mt-3">@{{ list.name }}</p>
-                <!-- <div class="card-scroll" data-flickity='{ "groupCells": true }'> -->
-                <div class="card-scroll">
-                    <div class="card" v-for="product in list.products">
-                        <div class="card-image-container">
-                            <img class="card-image" :src="product.image?.url" alt="img">
-                        </div>
-                        <div class="card-detail">
-                            <span class="menu-name">@{{ product.name }}</span>
-                            <span class="main-price">@{{formatCurrency(product.price)}}</span>
-                            <hr class="hr-style">
-                            <p class="menu-detail">@{{ product.description }}</p>
-                            <button class="order-detail-btn" data-target="#product-detail-modal" data-toggle="modal"
-                                @click="getDetail(product)">
-                                <span>Order Detail</span>
-                                <span class="order-detail-btn-arrow"><i class="fa fa-arrow-right"></i></span>
-                            </button>
-                        </div>
-                        <!-- @include('home.modal.productdetails') -->
-                    </div>
-                </div>
-            </section>
-
-            @include('home.modal.cart')
+<div class="container" id="homePage" v-cloak>
+    <nav class="navbar">
+        <div class="navbar-brand">
+            <a href="#"><img class="logo" src="{{ asset('images/amboja_logo.jpg') }}" alt="ambojaLogo"></a>
         </div>
         <ul class="navbar-menu">
             <div v-if="carts">
@@ -113,8 +65,8 @@
     const data = @json($data);
 
 </script>
-<script src="{{ mix('public/dist/js/app.js') }}"></script>
-<script src="{{ mix('public/dist/js/home/index.js') }}"></script>
+<script src="{{ mix('dist/js/app.js') }}"></script>
+<script src="{{ mix('dist/js/home/index.js') }}"></script>
 <script>
     $(document).ready(function () {
         $('#myModal').on('show.bs.modal', function () {
@@ -132,8 +84,6 @@
         });
         $('#btn-print-invoice').click(function() {
             showLoading();
-            $('#btn-print-invoice').css('display', 'none');
-            $('#btn-close-invoice').css('display', 'none');
             html2canvas($('#invoiceModal')[0]).then(canvas => {
                 hideLoading();
                 var dataString = canvas.toDataURL("image/png");
@@ -141,12 +91,7 @@
                 link.download = 'image';
                 link.href = dataString;
                 link.click();
-
             });
-            setTimeout(function() {
-                window.location.reload();
-            }, 10000);
-
         })
 
         function printElement(elem) {
@@ -168,6 +113,8 @@
             }, 250);
 
         }
-    });
-</script>
 
+    });
+
+</script>
+@endsection
